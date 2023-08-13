7500fe55b78cc4ae5ef3dd96ccf1e7d39070dedc
Shobhit Kushwaha <shobhitkushwaha1406@gmail.com>
2023-08-13 16:10:13 +0530

    login page refactored

--- grid/pages/login.js
+++ grid/pages/login.js
@@ -97,29 +97,29 @@ export default function Login() {
                                 <h1 className='text-blue-950 font-extrabold text-3xl mb-7'>NextZone Login</h1>
                                 <div className='flex flex-col gap-1 mb-2 items-center' >
 
-                                    <span className='flex gap-3'>
+                                    <span className='flex gap-3 ml-1 mr-auto' >
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z" />
                                         </svg>
                                         E-mail
                                     </span>
                                     <input type="text" placeholder='Enter Your Email' value={email} onChange={e => setEmail(e.target.value)}
-                                        className=' pl-6 min-w-[300px] w-[30%] h-[2.5rem] rounded-xl focus:outline-none border-2 border-gray-300 mt-1'
+                                        className=' pl-6 min-w-[300px] w-[30%] h-[2.5rem] rounded-md focus:outline-none border-2 border-gray-300 mt-1'
                                     />
                                 </div>
 
                                 <div className='flex flex-col gap-1 mb-2 items-center'>
 
-                                    <span className='flex gap-3'>
+                                    <span className='flex gap-3 ml-1 mr-auto'>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
                                         </svg>
 
                                         Password
                                     </span>
-                                    <input type="password" placeholder='Enter Your Password' value={password} onChange={e => setPassword(e.target.value)} className=' pl-6 min-w-[300px] w-[30%] h-[2.5rem] rounded-xl focus:outline-none border-2 border-gray-300 mt-1' />
+                                    <input type="password" placeholder='Enter Your Password' value={password} onChange={e => setPassword(e.target.value)} className=' pl-6 min-w-[300px] w-[30%] h-[2.5rem] rounded-md focus:outline-none border-2 border-gray-300 mt-1' />
                                 </div>
-                                <button type='submit' className="w-[100%] md:w-[100%] bg-slate-700 text-white font-bold h-[2.5rem] rounded-xl hover:border  hover:border-blue-950 hover:text-blue-950 hover:bg-white ease-in-out duration-300 flex justify-center items-center gap-4 hover:gap-7">
+                                <button type='submit' className="w-[100%] md:w-[100%] bg-neutral-800 text-white font-bold h-[2.5rem] rounded-md hover:border   hover:border-blue-950 hover:text-gray-200 hover:bg-neutral-600 ease-in-out duration-300 flex justify-center items-center gap-4 hover:gap-7">
                                     <span>
                                         Login
                                     </span>
@@ -128,7 +128,7 @@ export default function Login() {
                                     </svg>
 
                                 </button>
-                                <Link href={'https://ecom-admin-panel.vercel.app/'} className="w-[100%] md:w-[100%] bg-slate-700 text-white font-bold h-[2.5rem] rounded-xl hover:border  hover:border-blue-950 hover:text-blue-950 hover:bg-white ease-in-out duration-300 flex justify-center items-center gap-4 hover:gap-7">
+                                <Link href={'https://ecom-admin-panel.vercel.app/'} className="w-[100%] md:w-[100%] bg-neutral-800 text-white font-bold h-[2.5rem] rounded-md hover:border  hover:border-blue-950 hover:text-gray-200 hover:bg-neutral-600 ease-in-out duration-300 flex justify-center items-center gap-4 hover:gap-7">
                                     <span>
                                         Admin Login
                                     </span>
--- grid/pages/register.js
+++ grid/pages/register.js
@@ -67,7 +67,7 @@ export default function Register() {
                             <form onSubmit={handleRegister} className='flex gap-1 flex-col'>
                                 <h1 className='text-blue-950 font-extrabold text-3xl mb-7 p-[-100px]'>NextZone Register</h1>
                                 <div className='flex flex-col gap-1 mb-2'>
-                                    <label className='flex gap-1 items-center justify-center' >
+                                    <label className='flex gap-1 items-center ml-1' >
 
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
@@ -75,38 +75,38 @@ export default function Register() {
                                         Name
 
                                     </label >
-                                    <input type="text" placeholder='Enter Your Name' value={name} onChange={e => setName(e.target.value)} className=" pl-6 min-w-[300px] w-[30%] h-[2.5rem] rounded-xl focus:outline-none border-2 border-gray-300" />
+                                    <input type="text" placeholder='Enter Your Name' value={name} onChange={e => setName(e.target.value)} className=" pl-6 min-w-[300px] w-[30%] h-[2.5rem] rounded-md focus:outline-none border-2 border-gray-300" />
                                 </div>
 
 
                                 <div className='flex flex-col gap-1 mb-2'>
-                                    <label className='flex gap-1 items-center justify-center' >
+                                    <label className='flex gap-1 items-center  ml-1' >
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                             <path strokeLinecap="round" d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25" />
                                         </svg>
                                         Email
 
                                     </label>
-                                    <input type="text" placeholder='Enter Your Email' value={email} onChange={e => setEmail(e.target.value)} className=" pl-6 min-w-[300px] w-[30%] h-[2.5rem] rounded-xl focus:outline-none border-2 border-gray-300" />
+                                    <input type="text" placeholder='Enter Your Email' value={email} onChange={e => setEmail(e.target.value)} className=" pl-6 min-w-[300px] w-[30%] h-[2.5rem] rounded-md focus:outline-none border-2 border-gray-300" />
                                 </div>
 
 
                                 <div className='flex flex-col gap-1 mb-2'>
 
-                                    <label className='flex gap-1 items-center justify-center'  >
+                                    <label className='flex gap-1 items-center  ml-1'  >
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z" />
                                         </svg>
                                         Password
 
                                     </label>
-                                    <input type="password" placeholder='Enter Your Password' value={password} onChange={e => setPassword(e.target.value)} className=" pl-6 min-w-[300px] w-[30%] h-[2.5rem] rounded-xl focus:outline-none border-2 border-gray-300" />
+                                    <input type="password" placeholder='Enter Your Password' value={password} onChange={e => setPassword(e.target.value)} className=" pl-6 min-w-[300px] w-[30%] h-[2.5rem] rounded-md focus:outline-none border-2 border-gray-300" />
 
                                 </div>
 
                                 <div className='flex flex-col gap-1 mb-2'>
 
-                                    <label className='flex gap-1 items-center justify-center'>
+                                    <label className='flex gap-1 items-center  ml-1'>
 
 
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -115,21 +115,21 @@ export default function Register() {
                                         Phone Number
 
                                     </label>
-                                    <input type="text" placeholder='Enter Your Phone Number' value={phoneNum} onChange={e => setPhoneNum(e.target.value)} className=" pl-6 min-w-[300px] w-[30%] h-[2.5rem] rounded-xl focus:outline-none border-2 border-gray-300" />
+                                    <input type="text" placeholder='Enter Your Phone Number' value={phoneNum} onChange={e => setPhoneNum(e.target.value)} className=" pl-6 min-w-[300px] w-[30%] h-[2.5rem] rounded-md focus:outline-none border-2 border-gray-300" />
                                 </div>
 
 
                                 <div className='flex flex-col gap-1 mb-2'>
-                                    <label className='flex gap-1 items-center justify-center'>
+                                    <label className='flex gap-1 items-center  ml-1' >
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
                                         </svg>
                                         Address
                                     </label>
-                                    <input type="text" placeholder='Enter Your Address' value={address} onChange={e => setAddress(e.target.value)} className=" pl-6 min-w-[300px] w-[30%] h-[2.5rem] rounded-xl focus:outline-none border-2 border-gray-300" />
+                                    <input type="text" placeholder='Enter Your Address' value={address} onChange={e => setAddress(e.target.value)} className=" pl-6 min-w-[300px] w-[30%] h-[2.5rem] rounded-md focus:outline-none border-2 border-gray-300" />
 
                                 </div>
-                                <button type='submit' className="w-[100%] md:w-[100%] bg-slate-700 text-white font-bold h-[2.5rem] rounded-xl hover:border  hover:border-blue-950 hover:text-blue-950 hover:bg-white ease-in-out duration-300 flex justify-center items-center gap-4 hover:gap-7" >
+                                <button type='submit' className="w-[100%] md:w-[100%] bg-slate-700 text-white font-bold h-[2.5rem] rounded-md hover:border  hover:border-blue-950 hover:text-blue-950 hover:bg-white ease-in-out duration-300 flex justify-center items-center gap-4 hover:gap-7" >
                                     <span>
                                         Register
                                     </span>
