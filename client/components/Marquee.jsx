8a3b46fbe578eee4cd628277f3f0575b675a5157
RishabhSharma96 <114917946+RishabhSharma96@users.noreply.github.com>
2023-08-15 00:04:19 +0530

    customization pages added

--- client/components/Marquee.jsx
+++ client/components/Marquee.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Marquee = () => {
     return (
         <>
-            <marquee onClick={() => window.open("https://customization-nextzone.netlify.app/", "_blank")} behavior="scroll" scrollamount="13" onmouseover="this.stop()" onmouseout="this.start()" direction="right" className="bg-neutral-800 cursor-pointer"  >
+            <marquee onClick={() => window.open("https://nextzone-customization.netlify.app/", "_blank")} behavior="scroll" scrollamount="13" onmouseover="this.stop()" onmouseout="this.start()" direction="right" className="bg-neutral-800 cursor-pointer"  >
                 <span className='text-gray-100 font-extrabold text-md'>
                 Introducing custom clothes! Finally, clothes that fit you perfectly and make you look your best. <span className='text-yellow-600'>TRY IT NOW</span>
                 </span>
