d10d4721bc005942afb384ab46ea4bfce49f1196
Ayush Sharma <ayushsharma170199@gmail.com>
2023-08-16 07:49:58 +0530

    adding App.jsx

---
+++ customWear/client/src/App.jsx
@@ -0,0 +1,17 @@
+import Canvas from "./canvas"
+import Home from "./pages/Home"
+import Customizer from './pages/Customizer'
+
+function App() {
+  
+
+  return (
+    <main className="app transition-all ease-in">
+      <Home/>
+      <Canvas/>
+      <Customizer/>
+    </main>
+  )
+}
+
+export default App
