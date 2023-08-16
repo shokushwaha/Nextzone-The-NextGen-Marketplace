d8a8c7c1689b7b2c50a20f799b14761870542106
Ayush Sharma <ayushsharma170199@gmail.com>
2023-08-16 07:56:21 +0530

    adding index.js

---
+++ customWear/client/src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from './CustomButton'
+import AIPicker from './AIPicker';
+import ColorPicker from './ColorPicker';
+import Tab from './Tab';
+import FilePicker from './FilePicker';
+
+export {CustomButton,Tab,FilePicker,ColorPicker,AIPicker};
